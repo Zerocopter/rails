be3886daa08c4c71bbd2167bfde48d6982de37e2
snicky <mariusz@zerocopter.com>
2020-08-19 17:05:06 +0800

    add simple rules

--- actionpack/lib/action_dispatch/http/resource_isolation_policy.rb
+++ actionpack/lib/action_dispatch/http/resource_isolation_policy.rb
@@ -10,13 +10,60 @@ module ActionDispatch #:nodoc:
         HTTP_SEC_FETCH_MODE = "HTTP_SEC_FETCH_MODE".freeze
         HTTP_SEC_FETCH_SITE = "HTTP_SEC_FETCH_SITE".freeze
         HTTP_SEC_FETCH_USER = "HTTP_SEC_FETCH_USER".freeze
+        SAME_ORIGIN = "same-origin".freeze
+        SAME_SITE = "same-site".freeze
+        NONE = "none".freeze
+        NAVIGATE = "navigate".freeze
+        GET = "get".freeze
+        OBJECT = "object".freeze
+        EMBED = "embed".freeze
 
         def initialize(request)
           @request = request
         end
 
-        def forbidden?
-          true
+        def allowed?
+          !sec_fetch_site || site_allowed? || get_navigation? || asset?
+        end
+
+        private
+
+        attr_reader :request
+
+        def site_allowed?
+          sec_fetch_site.in?([SAME_ORIGIN, SAME_SITE, NONE])
+        end
+
+        def get_navigation?
+          get? && navigate? && !object_or_embed
+        end
+
+        def asset?
+          request.original_fullpath.matches?(/\A\/assets\//)
+        end
+
+        def get?
+          request.method == GET
+        end
+
+        def navigate?
+          sec_fetch_mode == NAVIGATE
+        end
+
+        def object_or_embed?
+          sec_fetch_dest.in?([OBJECT, EMBED])
+        end
+
+        def sec_fetch_site
+          request.headers[HTTP_SEC_FETCH_SITE]
+        end
+
+        def sec_fetch_mode
+          request.headers[HTTP_SEC_FETCH_MODE]
+        end
+
+        def sec_fetch_dest
+          request.headers[HTTP_SEC_FETCH_DEST]
         end
       end
 
@@ -39,10 +86,10 @@ module ActionDispatch #:nodoc:
       def call(env)
         request = ActionDispatch::Request.new env
 
-        response_app = if Permissions.new(request).forbidden?
-          FORBIDDEN_RESPONSE_APP
-        else
+        response_app = if Permissions.new(request).allowed?
           @app
+        else
+          FORBIDDEN_RESPONSE_APP
         end
 
         response_app.call(env)
